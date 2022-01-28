c26ea99e45615e41d387f0779ff1ed8d66a52761
Burdinskaia <78086736+Burdinskaia@users.noreply.github.com>
2022-01-28 07:31:54 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+num_1 = int(input('Ievadiet pirmo skaitli: ')) #iespēja ievadīt pirmo skaitli
+num_2 = int(input('Ievadiet otro skaitli: ')) #iespēja ievadīt otro skaitli
+
+print('{} + {} = '.format(num_1, num_2)) #darbība ar mainīgajiem - skaitīšana
+print(num_1 + num_2) #izvadīt rezultātu uz ekrāna
+
+print('{} - {} = '.format(num_1, num_2)) #darbība ar mainīgajiem - atņemšana
+print(num_1 - num_2) #izvadīt rezultātu uz ekrāna
+
+print('{} * {} = '.format(num_1, num_2)) #darbība ar mainīgajiem - reizināšana
+print(num_1 * num_2) #izvadīt rezultātu uz ekrāna
+
+print('{} / {} = '.format(num_1, num_2)) #darbība ar mainīgajiem - dalīšana
+print(num_1 / num_2) #izvadīt rezultātu uz ekrāna
